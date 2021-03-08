fc1c5b0195ef27ab2c986d42c960766e84d08917
mattyoung <matt.young@appdetex.com>
2021-03-08 13:34:50 -0700

    cleaned up file structure

--- wwwsrc/vue.config.js
+++ wwwsrc/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
   publicPath: '/',
-  outputDir: '../keepr2/wwwroot',
+  outputDir: '../wwwroot',
   productionSourceMap: false
 }
